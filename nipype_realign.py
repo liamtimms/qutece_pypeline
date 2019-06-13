23dca6239fe404cb68e1c9cd10981d6abb4fc44d
timms5000 <timms5000@gmail.com>
2019-06-12 22:03:11 -0400

    added more templates, setting up for larger pipeline

--- nipype_realign.py
+++ nipype_realign.py
@@ -13,21 +13,39 @@ import nipype.interfaces.utility as utl
 import nipype.interfaces.io as nio
 
 # must define working_dir
-
 # working_dir =
 
+# Define subject list, session list and relevent file types
 subject_list = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11']
 session_list = ['Precon', 'Postcon', 'Blood']
 
-# 1. search for scans
+subdirectory = os.path.join('sub-{subject_id}', 'ses-{session_id}')
+filestart = 'sub-{subject_id}_ses-{session_id}_'
+
+scantype = 'qutece'
+qutece_highres_files = os.path.join(subdirectory, scantype,
+                                    filestart, '_hr_run*_UTE.nii')
+qutece_fast_files = os.path.join(subdirectory, scantype,
+                                    filestart, '_fast*_UTE.nii')
+
+scantype = 'anat'
+anat_files = os.path.join(subdirectory, scantype,
+                                    filestart, '_T1w.nii')
+swi_files = os.path.join(subdirectory, scantype,
+                                    filestart, '_SWI_angio.nii')
+
+templates = {'qutece-hr': qutece_highres_files,
+             'qutece_fast': qutece_fast_files,
+             'anat': anat_files,
+             'swi': swi_files}
+
+        # 1. search for scans
 # Infosource - a function free node to iterate over the list of subject names
 infosource = eng.Node(utl.IdentityInterface(fields=['subject_id', 'session_id']),
                   name="infosource")
 infosource.iterables = [('subject_id', subject_list),
                         ('session_id', session_list)]
 
-templates = {'qutece': os.path.join('sub-{subject_id}/ses-{session_id}/qutece/',
-                     'sub-{subject_id}_ses-{session_id}_hr_run-*_UTE.nii.gz')}
 
 selectfiles = eng.Node(nio.SelectFiles(templates,
                                base_directory=working_dir,
@@ -73,7 +91,7 @@ realign_wf = eng.Workflow(name = 'Intrascan_Realign')
 realign_wf.base_dir = working_dir + '/workflow'
 
 realign_wf.connect([(infosource, selectfiles, [('subject_id', 'subject_id'),
-                                              ('session_id', 'session_id')],
+                                              ('session_id', 'session_id')]),
                     (selectfiles, intrascan_realign,  [('qutece', 'in_files')])])
 
 realign_wf.connect([(intrascan_realign, datasink,
