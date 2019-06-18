83e1caccc30d16c5a580481395ffcb12dbea34c9
timms5000 <timms5000@gmail.com>
2019-06-18 07:59:52 -0400

    lots of debugging and also cut down to only complete patients

--- nipype_preproc.py
+++ nipype_preproc.py
@@ -1,7 +1,7 @@
 # Preprocessing Pipeline
 # -----------------Imports-------------------------------
 import os
-import CustomNipype as cnp
+import CustomNiPype as cnp
 import nipype.pipeline.engine as eng
 import nipype.interfaces.spm as spm
 import nipype.interfaces.freesurfer as fs
@@ -17,28 +17,22 @@ working_dir = os.path.abspath('/mnt/hgfs/VMshare/WorkingBIDS/')
 output_dir = os.path.join(working_dir, 'derivatives/')
 temp_dir = os.path.join(output_dir, 'datasink/')
 
-subject_list = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11']
+#subject_list = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11']
+subject_list = ['02', '03', '04', '06', '07', '08', '09', '10', '11']
 session_list = ['Precon', 'Postcon', 'Blood']
 
 subdirectory = os.path.join('sub-{subject_id}', 'ses-{session_id}')
-filestart = 'sub-{subject_id}_ses-{session_id}_'
+filestart = 'sub-{subject_id}_ses-{session_id}'
 
 scantype = 'qutece'
 qutece_highres_files = os.path.join(subdirectory, scantype,
-                                    filestart, '_hr_run*_UTE.nii')
-qutece_fast_files = os.path.join(subdirectory, scantype,
-                                    filestart, '_fast*_UTE.nii')
+                                    filestart + '_hr*_UTE.nii')
+#qutece_fast_files = os.path.join(subdirectory, scantype,
+#                                    filestart + '_fast*_UTE.nii')
 
-scantype = 'anat'
-anat_files = os.path.join(subdirectory, scantype,
-                                    filestart, '_T1w.nii')
-swi_files = os.path.join(subdirectory, scantype,
-                                    filestart, '_SWI_angio.nii')
 
-templates = {'qutece_hr': qutece_highres_files,
-             'qutece_fast': qutece_fast_files,
-             'T1w': anat_files,
-             'swi': swi_files}
+templates = {'qutece_hr': qutece_highres_files}
+#             'qutece_fast': qutece_fast_files}
 
 # Infosource - a function free node to iterate over the list of subject names
 infosource = eng.Node(utl.IdentityInterface(fields=['subject_id', 'session_id']),
@@ -50,7 +44,7 @@ infosource.iterables = [('subject_id', subject_list),
 selectfiles = eng.Node(nio.SelectFiles(templates,
                                base_directory=working_dir,
                                sort_filelist=True, raise_on_empty=True),
-                   name="Select Files")
+                   name="SelectFiles")
 # -------------------------------------------------------
 
 # -----------------------UnringNode----------------------
@@ -59,8 +53,8 @@ unring_nii = eng.MapNode(interface = cnp.UnringNii(),
 # -------------------------------------------------------
 
 # -----------------------BiasFieldCorrection-------------
-bias_norm = eng.Node(ants.N4BiasFieldCorrection(),
-                     name = 'Bias Correction')
+bias_norm = eng.MapNode(ants.N4BiasFieldCorrection(),
+                     name = 'BiasCorrection', iterfield=['input_image'])
 # -------------------------------------------------------
 
 # ------------------------Output-------------------------
@@ -70,19 +64,19 @@ datasink = eng.Node(nio.DataSink(base_directory=output_dir,
                 name="datasink")
 # Use the following DataSink output substitutions
 substitutions = [('_subject_id_', 'sub-'), ('_session_id_', 'ses-')]
-subjFolders = [('sub-%s_session_id_%s' % (sub, ses), 'sub-%s/ses-%s' % (sub, ses))
+subjFolders = [('ses-%ssub-%s' % (ses, sub), ('sub-%s/ses-%s/'+scantype) % (sub, ses))
                for ses in session_list
                for sub in subject_list]
 substitutions.extend(subjFolders)
 datasink.inputs.substitutions = substitutions
+datasink.inputs.regexp_substitutions = [('_BiasCorrection.','')]
 # -------------------------------------------------------
 
 # -----------------UnringWorkflow------------------------
-preproc_wf = eng.Workflow(name = 'Preprocessing')
+preproc_wf = eng.Workflow(name = 'Preprocessing', base_dir = working_dir + '/workflow')
 preproc_wf.connect([(infosource, selectfiles, [('subject_id', 'subject_id'),
                                              ('session_id', 'session_id')]),
-                  (selectfiles, unring_nii, [('qutece_hr', 'in_file'),
-                                             ('qutece_fast', 'in_file')]),
+                  (selectfiles, unring_nii, [('qutece_hr', 'in_file')]),
                   (unring_nii, bias_norm, [('out_file', 'input_image')]),
                   (bias_norm, datasink, [('output_image', 'preproc.@con')])
                   ])
