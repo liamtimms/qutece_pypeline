8162bd5d29232c80eeb73b313bbe4656ee92e71a
timms5000 <timms5000@gmail.com>
2019-06-20 20:15:33 -0400

    changed to a notT1w glob template for source files of precon coreg

--- nipype_coreg.py
+++ nipype_coreg.py
@@ -6,8 +6,6 @@ import CustomNiPype as cnp
 import nipype.pipeline.engine as eng
 import nipype.interfaces.spm as spm
 import nipype.interfaces.freesurfer as fs
-import nipype.interfaces.fsl as fsl
-import nipype.interfaces.ants as ants
 import nipype.interfaces.utility as utl
 import nipype.interfaces.io as nio
 # -------------------------------------------------------
@@ -19,7 +17,7 @@ output_dir = os.path.join(working_dir, 'derivatives/')
 temp_dir = os.path.join(output_dir, 'datasink/')
 
 subject_list = ['02', '03', '06', '07', '08', '09', '10', '11']
-session_list = ['Precon'] 
+session_list = ['Precon']
 
 subdirectory = os.path.join('sub-{subject_id}', 'ses-{session_id}')
 filestart = 'sub-{subject_id}_ses-{session_id}'
@@ -28,15 +26,11 @@ scantype = 'anat'
 T1w_files = os.path.join(subdirectory, scantype,
                                     filestart + '_T1w.nii')
 
-FLAIR_files = os.path.join(subdirectory, scantype,
-                                    filestart + '_FLAIR.nii')
-
-SWI_files = os.path.join(subdirectory, scantype,
-                                    filestart + '_SWI_angio.nii')
+nonT1w_files = os.path.join(subdirectory, scantype,
+                                    filestart + '_[!T1w]*.nii')
 
 templates = {'T1w': T1w_files,
-             'FLAIR': FLAIR_files,
-             'SWI': SWI_files}
+             'notT1w': FLAIR_files}
 
 # Infosource - a function free node to iterate over the list of subject names
 infosource = eng.Node(utl.IdentityInterface(fields=['subject_id', 'session_id']),
@@ -65,7 +59,7 @@ datasink = eng.Node(nio.DataSink(base_directory=output_dir,
 # Use the following DataSink output substitutions
 substitutions = [('_subject_id_', 'sub-'), ('_session_id_', 'ses-')]
 
-subjFolders = [('ses-%ssub-%s' % (ses, sub), 
+subjFolders = [('ses-%ssub-%s' % (ses, sub),
                 'sub-%s/ses-%s' % (sub, ses))
                for ses in session_list
                for sub in subject_list]
@@ -78,15 +72,13 @@ task = 'CoregisterPrecon'
 coreg_wf = eng.Workflow(name = task)
 coreg_wf.base_dir = working_dir + '/workflow'
 
-# I don't expect this to work... TODO: make select files pass a list of non-T1w 
+# I don't expect this to work... TODO: make select files pass a list of non-T1w
 # files to the coreg mapnode and then use source as the iterable
 coreg_wf.connect([(infosource, selectfiles, [('subject_id', 'subject_id'),
                                               ('session_id', 'session_id')])])
 
 coreg_wf.connect([(selectfiles, coreg, [('T1w', 'target'),
-                                          'FLAIR', 'source'])])
-coreg_wf.connect([(selectfiles, coreg, [('T1w', 'target'),
-                                          'SWI', 'source'])])
+                                          'notT1w', 'source'])])
 coreg_wf.connect([(coreg, datasink,
                      [('coregistered_source', task+'.@con')])])
 # -------------------------------------------------------
