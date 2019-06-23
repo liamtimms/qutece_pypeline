ab8efe5a38e67a2355f63f058b1adc1d5c9c859e
timms5000 <timms5000@gmail.com>
2019-06-23 18:15:15 -0400

    did a ton of work on coreg2 aka intersession coregistration, need to test if the merge utility node works

--- nipype_coreg2.py
+++ nipype_coreg2.py
@@ -16,36 +16,71 @@ working_dir = os.path.abspath('/mnt/hgfs/VMshare/WorkingBIDS/')
 output_dir = os.path.join(working_dir, 'derivatives/')
 temp_dir = os.path.join(output_dir, 'datasink/')
 
+#TODO: Change this so that it is not iterating over subject and session 
+#      but rather, just over the subject with selectfiles grabbing pre or 
+#      post-con as appropriate
 subject_list = ['02', '03', '05', '06', '08', '10', '11']
-session_list = ['Precon']
 
-subdirectory = os.path.join('sub-{subject_id}', 'ses-{session_id}')
-filestart = 'sub-{subject_id}_ses-{session_id}'
 
+# session_list = ['Precon', 'Postcon']
+
+# TODO: grab;
+
+# * realigned precontrast average
+scantype = 'qutece'
+session = 'Precon'
+subdirectory = os.path.join(temp_dir, 'realignmean',
+                            'sub-{subject_id}', 'ses-'+session)
+filestart = 'sub-{subject_id}_ses-'+ session +'_'
+qutece_mean_precon_file = os.path.join(subdirectory,
+                                       'mean'+filestart+'hr_run*.nii')
+
+# * realigned precontrast scans
+subdirectory = os.path.join(temp_dir, 'realign1',
+                            'sub-{subject_id}', 'ses-'+session)
+filestart = 'sub-{subject_id}_ses-'+ session +'_'
+qutece_precon_files = os.path.join(subdirectory,
+                                       'r'+filestart+'hr_run*.nii')
+
+# * realigned postcontrast average
+session = 'Postcon'
+subdirectory = os.path.join(temp_dir, 'realignmean',
+                            'sub-{subject_id}', 'ses-'+session)
+filestart = 'sub-{subject_id}_ses-'+ session +'_'
+qutece_mean_postcon_file = os.path.join(subdirectory,
+                                       'mean'+filestart+'hr_run*.nii')
+
+
+# directory: '\WorkingBIDS\derivatives\datasink\IntrasessionCoregister_T1w\sub-11\ses-Precon'
+
+# * precon IntrasessionCoregister_nonT1w
 scantype = 'anat'
-T1w_files = os.path.join(subdirectory, scantype,
-                                    filestart + '_T1w.nii')
+session = 'Precon'
+subdirectory = os.path.join(temp_dir, 'IntrasessionCoregister_T1w',
+                            'sub-{subject_id}', 'ses-'+session)
+filestart = 'sub-{subject_id}_ses-'+ session +'_'
+T1w_files  = os.path.join(subdirectory,
+                                       'r'+filestart+'*.nii')
 
-nonT1w_files = os.path.join(subdirectory, scantype,
-                                    filestart + '_[!T1w]*.nii')
+# * precon IntrasessionCoregister_T1w
+subdirectory = os.path.join(temp_dir, 'IntrasessionCoregister_nonT1w',
+                            'sub-{subject_id}', 'ses-'+session)
+nonT1w_files  = os.path.join(subdirectory,
+                                       'r'+filestart+'*.nii')
 
-templates = {'T1w': T1w_files,
+
+
+templates = {'qutece_precon_mean': qutece_mean_precon_file,
+             'qutece_precon'      : qutece_precon_files, 
+             'qutece_postcon_mean': qutece_mean_postcon_file,
+             'T1w': T1w_files,
              'nonT1w': nonT1w_files}
 
-subdirectory = os.path.join(temp_dir, 'realignmean',
-                            'sub-{subject_id}', 'ses-{session_id}')
-filestart = 'mean'+'sub-{subject_id}_ses-{session_id}_'
-
-scantype = 'qutece'
-qutece_highres_files = os.path.join(subdirectory,
-                                    filestart+'hr_run*.nii')
-templates = {'qutece_mean': qutece_highres_files}
 
 # Infosource - a function free node to iterate over the list of subject names
-infosource = eng.Node(utl.IdentityInterface(fields=['subject_id', 'session_id']),
+infosource = eng.Node(utl.IdentityInterface(fields=['subject_id']),
                   name="infosource")
-infosource.iterables = [('subject_id', subject_list),
-                        ('session_id', session_list)]
+infosource.iterables = [('subject_id', subject_list)]
 
 # Selectfiles to provide specific scans with in a subject to other functions
 selectfiles = eng.Node(nio.SelectFiles(templates,
@@ -55,10 +90,17 @@ selectfiles = eng.Node(nio.SelectFiles(templates,
 # -------------------------------------------------------
 
 # -----------------------CoregisterNodes-----------------
-= eng.Node(spm.Coregister(), name = 'coreg2')
-coreg2.inputs.write_interp = 7
-coreg2.inputs.separation = [6, 3, 2]
+#coreg_to_postcon = eng.JoinNode(spm.Coregister(), name = 'coreg_to_postcon', joinsource= 'selectfiles', joinfield = 'apply_to_files')
+coreg_to_postcon = eng.Node(spm.Coregister(), name = 'coreg_to_postcon') 
+coreg_to_postcon.inputs.write_interp = 7
+coreg_to_postcon.inputs.separation = [6, 3, 2]
 # -------------------------------------------------------
+
+# -----------------------Merge---------------------------
+merge = eng.Node(utl.Merge(3), name = 'merge')
+merge.ravel_inputs = True
+# -------------------------------------------------------
+
 
 # ------------------------Output-------------------------
 # Datasink - creates output folder for important outputs
@@ -66,28 +108,38 @@ datasink = eng.Node(nio.DataSink(base_directory=output_dir,
                          container=temp_dir),
                 name="datasink")
 # Use the following DataSink output substitutions
-substitutions = [('_subject_id_', 'sub-'), ('_session_id_', 'ses-')]
+substitutions = [('_subject_id_', 'sub-')]
 
-subjFolders = [('ses-%ssub-%s' % (ses, sub),
-                'sub-%s/ses-%s' % (sub, ses))
-               for ses in session_list
-               for sub in subject_list]
+subjFolders = [('sub-%s' % (sub),
+                'sub-%s/ses-Precon' % (sub))
+               for ses in session_list]
 substitutions.extend(subjFolders)
 datasink.inputs.substitutions = substitutions
+datasink.inputs.regexp_substitutions = [('_coreg_to_postcon.','')]
 # -------------------------------------------------------
 
 # -----------------CoregistrationWorkflow----------------
-task = 'IntrasessionCoregister2'
+task = 'IntersessionCoregister'
 coreg_wf = eng.Workflow(name = task)
 coreg_wf.base_dir = working_dir + '/workflow'
 
 coreg_wf.connect([(infosource, selectfiles, [('subject_id', 'subject_id'),
                                               ('session_id', 'session_id')])])
 
-coreg_wf.connect([(selectfiles, coreg2, [('qutece_mean', 'target'),
-                                          ('T1w', 'source')])])
-coreg_wf.connect([(coreg2, datasink,
-                     [('coregistered_source', task+'.@con'),
-                     ('coregistered_files', task+'2.@con')])])
+coreg_wf.connect([(selectfiles, merge, [('qutece_precon_files', 'in1'),
+                                        ('T1w', 'in2'),
+                                        ('nonT1w', 'in3')])])
+
+
+coreg_wf.connect([(selectfiles, coreg_to_postcon, [('qutece_precon_mean', 'target'),
+                                                   ('qutece_postcon_mean', 'source')
+                                                   ])])
+
+coreg_wf.connect([(merge, coreg_to_postcon, [('out', 'apply_to_files')])])
+
+coreg_wf.connect([(coreg_to_postcon, datasink,
+                     [('coregistered_source', task+'_nonT1w.@con')])])
 
 # -------------------------------------------------------
+
+
