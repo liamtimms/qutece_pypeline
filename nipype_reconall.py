7d72f88c6fee71df3e24dcda37cd76aaa8be3c9b
timms5000 <timms5000@gmail.com>
2019-07-24 22:04:13 -0400

    actually that last commit was a huge change over to the pre-existing recon all work flow

--- nipype_reconall.py
+++ nipype_reconall.py
@@ -1,4 +1,5 @@
 
+import os
 from nipype.workflows.smri.freesurfer import create_reconall_workflow
 
 working_dir = os.path.abspath('/mnt/hgfs/VMshare/WorkingBIDS/')
@@ -6,17 +7,17 @@ output_dir = os.path.join(working_dir, 'derivatives/')
 temp_dir = os.path.join(output_dir, 'datasink/')
 #subject_list = ['02', '03', '05', '06', '08', '10', '11']
 #subject_list =['11']
-subject = 11
+subject = '11'
 
 session = 'Precon'
 # * precon T1w from IntersessionCoregister_preconScans
 scanfolder = 'IntersessionCoregister_preconScans'
 subdirectory = os.path.join(temp_dir, scanfolder)
 filestart = 'sub-'+ subject +'_ses-'+ session +'_'
-precon_T1w_files  = 'rr'+filestart+'T1w.nii'
+precon_T1w_files  = os.path.join(subdirectory,'sub-'+subject,'rr'+filestart+'T1w.nii')
 
 recon_all = create_reconall_workflow()
 recon_all.inputs.inputspec.subject_id = 'sub-'+subject
 recon_all.inputs.inputspec.subjects_dir = subdirectory
 recon_all.inputs.inputspec.T1_files = precon_T1w_files 
-recon_all.run()
+
