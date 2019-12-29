0fa1e5d3729240f14e2875c0cf4935dea33d8ac2
Liam Timms <timms5000@gmail.com>
2019-12-29 13:12:17 -0500

    probably just changed formatting via yapf

--- time-series.py
+++ time-series.py
@@ -9,17 +9,22 @@ subject_list = ['02', '03', '05', '06', '08', '10', '11']
 #sub_num = '11'
 
 for sub_num in subject_list:
-    brain_ROI_filename = os.path.join(base_path, 'manualwork', 'brain_seg', 'sub-' + sub_num +
-            '_ses-Difference_fast-task-rest_brain-label.nii')
+    brain_ROI_filename = os.path.join(
+        base_path, 'manualwork', 'brain_seg',
+        'sub-' + sub_num + '_ses-Difference_fast-task-rest_brain-label.nii')
     brain_ROI_nii = nib.load(brain_ROI_filename)
     brain_ROI_img = np.array(brain_ROI_nii.get_data())
 
-    postminuspre_directory = os.path.join(base_path, 'datasink','postminuspre','sub-' + sub_num)
-    num_runs = len([name for name in os.listdir(postminuspre_directory) if os.path.isfile(os.path.join(postminuspre_directory, name))])
-    precon_scan = 'rmeansub-'+sub_num+'_ses-Precon_fast-task-rest_run-01_desc-unring_UTE_corrected.nii'
-    brain_aves = [[0]*2 for i in range(num_runs)]
+    postminuspre_directory = os.path.join(base_path, 'datasink',
+                                          'postminuspre', 'sub-' + sub_num)
+    num_runs = len([
+        name for name in os.listdir(postminuspre_directory)
+        if os.path.isfile(os.path.join(postminuspre_directory, name))
+    ])
+    precon_scan = 'rmeansub-' + sub_num + '_ses-Precon_fast-task-rest_run-01_desc-unring_UTE_corrected.nii'
+    brain_aves = [[0] * 2 for i in range(num_runs)]
     print(num_runs)
-    for run_num in range(1,num_runs+1):
+    for run_num in range(1, num_runs + 1):
         if run_num < 10:
             str_run_num = '0' + str(run_num)
         else:
@@ -27,9 +32,10 @@ for sub_num in subject_list:
 
         print(str_run_num)
 
-        postminuspre_filename = os.path.join(postminuspre_directory, 'rsub-' +
-                sub_num + '_ses-Postcon_fast-task-rest_run-' + str_run_num +
-                '_desc-unring_UTE_corrected_minus_'+ precon_scan)
+        postminuspre_filename = os.path.join(
+            postminuspre_directory,
+            'rsub-' + sub_num + '_ses-Postcon_fast-task-rest_run-' +
+            str_run_num + '_desc-unring_UTE_corrected_minus_' + precon_scan)
 
         postminuspre_nii = nib.load(postminuspre_filename)
         postminuspre_nii.set_data_dtype(np.double)
@@ -38,13 +44,11 @@ for sub_num in subject_list:
         brain_crop_img = np.multiply(postminuspre_img, brain_ROI_img)
         brain_vals = np.reshape(brain_crop_img, -1)
         brain_ave = np.nanmean(brain_vals[np.nonzero(brain_vals)])
-    #    brain_aves.append(brain_ave)
-        brain_aves[run_num-1][1]=brain_ave
-        brain_aves[run_num-1][0]=run_num
+        #    brain_aves.append(brain_ave)
+        brain_aves[run_num - 1][1] = brain_ave
+        brain_aves[run_num - 1][0] = run_num
 
     ave_filename = os.path.join(base_path, 'manualwork', 'timeseries',
-            'sub-' + sub_num + '_brain-aves.csv')
+                                'sub-' + sub_num + '_brain-aves.csv')
     export = pd.DataFrame(brain_aves)
     export.to_csv(ave_filename, index=False)
-
-
