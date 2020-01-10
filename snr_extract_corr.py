f261dc1d33233db9bc718540e7272abc0519bdcb
Liam Timms <timms5000@gmail.com>
2020-01-09 19:38:06 -0500

    added extraction for multiple parameters and added a seperate extraction code for the SNR of the corrected scans

--- snr_extract_corr.py
+++ snr_extract_corr.py
@@ -12,16 +12,15 @@ subject_list = ['3', '4', '5', '6']
 #subject_list = ['3', '4']
 
 for sub_num in subject_list:
-    data_dir = os.path.join(base_path, 'sub-' + sub_num, 'ses-Post', 'qutece')
+    data_dir = os.path.join(base_path, 'derivatives', 'datasink',
+                            'preprocessing', 'sub-' + sub_num, 'ses-Post',
+                            'qutece')
+    json_dir = os.path.join(base_path, 'sub-' + sub_num, 'ses-Post', 'qutece')
     snr_mask_dir = os.path.join(base_path, 'derivatives', 'manual-work', 'snr',
                                 'sub-' + sub_num)
     n = 0
 
-    sub_vals = [[
-        'Scan Name', 'Blood Intensity', 'Intraluminal Signal Homogeneity',
-        'SNR', 'CNR', 'Voxel Size', 'TE', 'TR', 'FA'
-    ]]
-
+    sub_vals = [['Scan Name', 'Blood Intensity', 'SNR', 'CNR', 'Voxel Size', 'TE', 'TR', 'FA']]
     for filename in os.listdir(data_dir):
         if filename.endswith(".nii"):
             scan_filename = os.path.join(data_dir, filename)
@@ -33,7 +32,8 @@ for sub_num in subject_list:
             pixdim = scan_hdr['pixdim']
             voxelsize = pixdim[1] * pixdim[2] * pixdim[3]
             path, fname, ext = split_filename(filename)
-            jsonfilename = os.path.join(data_dir, fname + '.json')
+            fname = fname.replace('_corrected', '')
+            jsonfilename = os.path.join(json_dir, fname + '.json')
 
             with open(jsonfilename) as json_file:
                 j_obj = json.load(json_file)
@@ -48,17 +48,13 @@ for sub_num in subject_list:
             snr_ROI_nii = nib.load(snr_filename)
             snr_ROI_img = np.array(snr_ROI_nii.get_fdata())
 
-            corr_fname = fname + '_corrected.nii'
-            snr_ROI_nii = nib.load(snr_filename)
-            snr_ROI_img = np.array(snr_ROI_nii.get_fdata())
-
             out = ([int(sub_num), n])
 
             for r in np.unique(snr_ROI_img):
                 roi = (snr_ROI_img == r).astype(int)
                 roi = roi.astype('float')
                 roi[roi ==
-                    0] = np.nan  #zero can be true value so mask with nan
+                    0] = np.nan  # zero can be true value so mask with nan
 
                 crop_img = np.multiply(scan_img, roi)
                 vals = np.reshape(crop_img, -1)
@@ -70,26 +66,24 @@ for sub_num in subject_list:
                 print('std = ' + str(std))
                 out.append([ave, std])
                 if r == 1:
-                    blood_signal = ave
-                    blood_noise = std
+                    signal = ave
                 elif r == 2:
                     noise = std
                 elif r == 3:
                     tissue = ave
 
-            snr = blood_signal / noise
-            ish = blood_noise / blood_signal
+            snr = signal / noise
             snr_tissue = tissue / noise
             cnr = snr - snr_tissue
-            scan_vals = [fname, blood_signal, ish, snr, cnr, voxelsize, TE, TR, FA]
+            scan_vals = [fname, signal, snr, cnr, voxelsize, TE, TR, FA]
             sub_vals.append(scan_vals)
             print('for ' + snr_fname)
-            print('SNR = ' + str(snr))  #+ '\n')
+            print('SNR = ' + str(snr))
             print('CNR = ' + str(cnr) + '\n')
-            del blood_signal, ish, noise, tissue
+            del signal, noise, tissue
             n = n + 1
 
-        scan_vals_csv_filename = os.path.join(snr_mask_dir,
-                                              'snr_sub-' + sub_num + '.csv')
+        scan_vals_csv_filename = os.path.join(
+            snr_mask_dir, 'snr_sub-' + sub_num + '_corrected.csv')
         export = pd.DataFrame(sub_vals)
         export.to_csv(scan_vals_csv_filename, index=False)
