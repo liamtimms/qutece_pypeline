6ddcebae8e6daeb7a44b80b64bf4105dfb1c1e6e
Liam Timms <timms5000@gmail.com>
2020-03-24 19:10:47 -0400

    added the scan difference

--- nipype_controller.py
+++ nipype_controller.py
@@ -1,6 +1,9 @@
 import os
 from nipype_preproc import Preproc_workflow
 from nipype_coreg import IntrasesCoreg_workflow
+from nipype_coreg2 import IntersesCoreg_workflow
+from nipype_scan_diff import ScanDiff_workflow
+from textme import textme
 
 upper_dir = os.path.realpath('../..')
 working_dir = os.path.abspath(upper_dir)
@@ -9,10 +12,22 @@ session_list = ['Precon', 'Postcon']
 subject_list = ['03', '11']
 subject_list = ['02', '04', '08', '10', '11']
 subject_list = ['02', '03', '04', '06', '08', '09', '10', '11']
-subject_list = ['03', '04', '06', '08', '11']
-num_cores = 7
+# subject_list = ['06', '11']
 
+num_cores = 1
 Preproc_workflow(working_dir, subject_list, session_list, num_cores)
 os.system("notify-send Preprocessing done")
+# textme("done with Preprocessing")
+
+# Extremely important to not start too many FLIRTs in parrallel
 IntrasesCoreg_workflow(working_dir, subject_list, session_list, num_cores)
 os.system("notify-send IntrasessionCoregistration done")
+# textme("done with IntrasessionCoregistration")
+
+IntersesCoreg_workflow(working_dir, subject_list, num_cores)
+os.system("notify-send IntrasessionCoregistration done")
+
+scan_type='hr'
+ScanDiff_workflow(working_dir, subject_list, session_list, num_cores, scan_type)
+scan_type='fast'
+ScanDiff_workflow(working_dir, subject_list, session_list, num_cores, scan_type)
