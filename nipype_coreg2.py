068effa93b36369c83e67e7de4bbd55c98db0d93
Liam Timms <timms5000@gmail.com>
2020-03-24 19:09:41 -0400

    Additional work on turning these into functions

--- nipype_coreg2.py
+++ nipype_coreg2.py
@@ -1,126 +1,139 @@
 # Interscan Coregister Pipeline
 # -----------------Imports-------------------------------
 import os
-import CustomNiPype as cnp
+# import CustomNiPype as cnp
 import nipype.pipeline.engine as eng
 import nipype.interfaces.spm as spm
-import nipype.interfaces.freesurfer as fs
+# import nipype.interfaces.freesurfer as fs
+import nipype.interfaces.fsl as fsl
 import nipype.interfaces.utility as utl
 import nipype.interfaces.io as nio
 # -------------------------------------------------------
 
-# -----------------Inputs--------------------------------
-# Define subject list, session list and relevent file types
-upper_dir = os.path.realpath('../..')
-working_dir = os.path.abspath(upper_dir)
-output_dir = os.path.join(working_dir, 'derivatives/')
-temp_dir = os.path.join(output_dir, 'datasink/')
 
-subject_list = ['02', '03', '04', '06', '08', '09', '10', '11']
-subject_list = ['03', '11']
-# session_list = ['Precon', 'Postcon']
+def IntersesCoreg_workflow(working_dir, subject_list, num_cores):
+    # -----------------Inputs--------------------------------
+    # Define subject list, session list and relevent file types
 
-# * realigned precontrast average
-scantype = 'qutece'
-session = 'Precon'
-subdirectory = os.path.join(temp_dir, 'realignmean', 'sub-{subject_id}',
-                            'ses-' + session, scantype)
-filestart = 'sub-{subject_id}_ses-' + session + '_'
-qutece_mean_precon_file = os.path.join(subdirectory,
-                                       '*mean' + filestart + '*hr*UTE*.nii')
+    output_dir = os.path.join(working_dir, 'derivatives/')
+    temp_dir = os.path.join(output_dir, 'datasink/')
 
-# * realigned precontrast scans
-subdirectory = os.path.join(temp_dir, 'preprocessing', 'sub-{subject_id}',
-                            'ses-' + session, scantype)
-filestart = 'sub-{subject_id}_ses-' + session + '_'
-qutece_precon_files = os.path.join(subdirectory,
-                                   'r' + filestart + '*UTE*.nii')
+    # * realigned precontrast average
+    scantype = 'qutece'
+    session = 'Precon'
+    subdirectory = os.path.join(temp_dir, 'preprocessing', 'sub-{subject_id}',
+                                'ses-' + session, scantype)
+    filestart = 'sub-{subject_id}_ses-' + session + '_'
+    qutece_mean_precon_file = os.path.join(
+        subdirectory, 'rmean' + filestart + '*hr*UTE*.nii')
 
-# * realigned postcontrast average
-session = 'Postcon'
-subdirectory = os.path.join(temp_dir, 'realignmean', 'sub-{subject_id}',
-                            'ses-' + session, scantype)
-filestart = 'sub-{subject_id}_ses-' + session + '_'
-qutece_mean_postcon_file = os.path.join(subdirectory,
-                                        '*mean' + filestart + '*hr*UTE*.nii')
+    # * realigned precontrast scans
+    subdirectory = os.path.join(temp_dir, 'preprocessing', 'sub-{subject_id}',
+                                'ses-' + session, scantype)
+    filestart = 'sub-{subject_id}_ses-' + session + '_'
+    qutece_precon_files = os.path.join(subdirectory,
+                                       'r' + filestart + '*UTE*.nii')
 
-# directory: '\WorkingBIDS\derivatives\datasink\IntrasessionCoregister_T1w\sub-11\ses-Precon'
+    # * realigned postcontrast average
+    session = 'Postcon'
+    subdirectory = os.path.join(temp_dir, 'preprocessing', 'sub-{subject_id}',
+                                'ses-' + session, scantype)
+    filestart = 'sub-{subject_id}_ses-' + session + '_'
+    qutece_mean_postcon_file = os.path.join(
+        subdirectory, 'rmean' + filestart + '*hr*UTE*.nii')
 
-# * precon IntrasessionCoregister_T1w
-scantype = 'anat'
-session = 'Precon'
-subdirectory = os.path.join(temp_dir, 'IntrasessionCoregister',
-                            'sub-{subject_id}', 'ses-' + session)
-filestart = 'sub-{subject_id}_ses-' + session + '_'
-anat_files = os.path.join(subdirectory, 'r' + filestart + '*.nii')
+    # directory: '\WorkingBIDS\derivatives\datasink\IntrasessionCoregister_T1w\sub-11\ses-Precon'
 
-templates = {
-    'qutece_precon_mean': qutece_mean_precon_file,
-    'qutece_precon': qutece_precon_files,
-    'qutece_postcon_mean': qutece_mean_postcon_file,
-    'anat': anat_files
-}
+    # * precon IntrasessionCoregister_T1w
+    scantype = 'anat'
+    session = 'Precon'
+    subdirectory = os.path.join(temp_dir, 'IntrasessionCoregister',
+                                'sub-{subject_id}', 'ses-' + session)
+    filestart = 'sub-{subject_id}_ses-' + session + '_'
+    anat_files = os.path.join(subdirectory, 'r' + filestart + '*.nii')
 
-# Infosource - a function free node to iterate over the list of subject names
-infosource = eng.Node(utl.IdentityInterface(fields=['subject_id']),
-                      name="infosource")
-infosource.iterables = [('subject_id', subject_list)]
+    templates = {
+        'qutece_precon_mean': qutece_mean_precon_file,
+        'qutece_precon': qutece_precon_files,
+        'qutece_postcon_mean': qutece_mean_postcon_file,
+        'anat': anat_files
+    }
 
-# Selectfiles to provide specific scans with in a subject to other functions
-selectfiles = eng.Node(nio.SelectFiles(templates,
-                                       base_directory=working_dir,
-                                       sort_filelist=True,
-                                       raise_on_empty=True),
-                       name="SelectFiles")
-# -------------------------------------------------------
+    # Infosource - a function free node to iterate over the list of subject names
+    infosource = eng.Node(utl.IdentityInterface(fields=['subject_id']),
+                          name="infosource")
+    infosource.iterables = [('subject_id', subject_list)]
 
-# -----------------------CoregisterNodes-----------------
-#coreg_to_postcon = eng.JoinNode(spm.Coregister(), name = 'coreg_to_postcon', joinsource= 'selectfiles', joinfield = 'apply_to_files')
-coreg_to_postcon = eng.Node(spm.Coregister(), name='coreg_to_postcon')
-coreg_to_postcon.inputs.write_interp = 7
-coreg_to_postcon.inputs.separation = [6, 3, 2]
-# -------------------------------------------------------
+    # Selectfiles to provide specific scans with in a subject to other functions
+    selectfiles = eng.Node(nio.SelectFiles(templates,
+                                           base_directory=working_dir,
+                                           sort_filelist=True,
+                                           raise_on_empty=True),
+                           name="SelectFiles")
+    # -------------------------------------------------------
 
-# -----------------------Merge---------------------------
-merge = eng.Node(utl.Merge(2), name='merge')
-merge.ravel_inputs = True
-# -------------------------------------------------------
+    # -----------------------CoregisterNodes-----------------
+    #coreg_to_postcon = eng.JoinNode(spm.Coregister(), name = 'coreg_to_postcon', joinsource= 'selectfiles', joinfield = 'apply_to_files')
+    coreg_to_postcon = eng.Node(spm.Coregister(), name='coreg_to_postcon')
+    coreg_to_postcon.inputs.write_interp = 7
+    coreg_to_postcon.inputs.separation = [6, 3, 2]
 
-# ------------------------Output-------------------------
-# Datasink - creates output folder for important outputs
-datasink = eng.Node(nio.DataSink(base_directory=output_dir,
-                                 container=temp_dir),
-                    name="datasink")
-# Use the following DataSink output substitutions
-substitutions = [('_subject_id_', 'sub-')]
+    coreg_to_postcon2 = eng.Node(spm.Coregister(), name='coreg_to_postcon2')
+    coreg_to_postcon2.inputs.write_interp = 7
+    coreg_to_postcon2.inputs.separation = [6, 3, 2]
+    # -------------------------------------------------------
 
-datasink.inputs.substitutions = substitutions
-# -------------------------------------------------------
+    # -----------------LinearRegistration--------------------
+    flirt = eng.Node(fsl.FLIRT(), name='flirt')
+    flirt.inputs.dof = 6  # rigid body transform
+    flirt.inputs.output_type = 'NIFTI'
+    # -------------------------------------------------------
 
-# -----------------CoregistrationWorkflow----------------
-task = 'IntersessionCoregister'
-coreg2_wf = eng.Workflow(name=task)
-coreg2_wf.base_dir = working_dir + '/workflow'
+    # -----------------------Merge---------------------------
+    merge = eng.Node(utl.Merge(2), name='merge')
+    merge.ravel_inputs = True
+    # -------------------------------------------------------
 
-coreg2_wf.connect([(infosource, selectfiles, [('subject_id', 'subject_id')])])
+    # ------------------------Output-------------------------
+    # Datasink - creates output folder for important outputs
+    datasink = eng.Node(nio.DataSink(base_directory=output_dir,
+                                     container=temp_dir),
+                        name="datasink")
+    # Use the following DataSink output substitutions
+    substitutions = [('_subject_id_', 'sub-')]
 
-coreg2_wf.connect([(selectfiles, merge, [('qutece_precon', 'in1'),
-                                         ('anat', 'in2')])])
+    datasink.inputs.substitutions = substitutions
+    # -------------------------------------------------------
 
-coreg2_wf.connect([(selectfiles, coreg_to_postcon,
-                    [('qutece_postcon_mean', 'target'),
-                     ('qutece_precon_mean', 'source')])])
+    # -----------------CoregistrationWorkflow----------------
+    task = 'IntersessionCoregister'
+    coreg2_wf = eng.Workflow(name=task)
+    coreg2_wf.base_dir = working_dir + '/workflow'
 
-coreg2_wf.connect([(merge, coreg_to_postcon, [('out', 'apply_to_files')])])
+    coreg2_wf.connect([
+        (infosource, selectfiles, [('subject_id', 'subject_id')]),
+        (selectfiles, merge, [('qutece_precon', 'in1'), ('anat', 'in2')]),
+        (selectfiles, coreg_to_postcon, [('qutece_postcon_mean', 'target'),
+                                         ('qutece_precon_mean', 'source')]),
+        (merge, coreg_to_postcon, [('out', 'apply_to_files')]),
+        # (coreg_to_postcon, datasink,
+        #  [('coregistered_source', task + '_preconUTEmean.@con'),
+        #   ('coregistered_files', task + '_preconScans.@con')]),
+        (selectfiles, coreg_to_postcon2, [('qutece_postcon_mean', 'target')]),
+        (coreg_to_postcon, coreg_to_postcon2,
+         [('coregistered_source', 'source'),
+          ('coregistered_files', 'apply_to_files')]),
+        (coreg_to_postcon2, datasink,
+         [('coregistered_source', task + '_preconUTEmeanSPM_SPM.@con'),
+          ('coregistered_files', task + '_preconScansSPM_SPM.@con')]),
+    ])
+    # -------------------------------------------------------
 
-coreg2_wf.connect([(coreg_to_postcon, datasink,
-                    [('coregistered_source', task + '_preconUTEmean.@con'),
-                     ('coregistered_files', task + '_preconScans.@con')])])
+    # -------------------WorkflowPlotting--------------------
+    coreg2_wf.write_graph(graph2use='flat')
+    # -------------------------------------------------------
 
-# -------------------------------------------------------
-
-# -------------------WorkflowPlotting--------------------
-coreg2_wf.write_graph(graph2use='flat')
-# -------------------------------------------------------
-
-coreg2_wf.run(plugin='MultiProc', plugin_args={'n_procs': 5})
+    if num_cores < 2:
+        coreg2_wf.run()
+    else:
+        coreg2_wf.run(plugin='MultiProc', plugin_args={'n_procs': num_cores})
--- nipype_scan_diff.py
+++ nipype_scan_diff.py
@@ -6,89 +6,107 @@ import nipype.pipeline.engine as eng
 import nipype.interfaces.spm as spm
 import nipype.interfaces.freesurfer as fs
 import nipype.interfaces.fsl as fsl
+import nipype.interfaces.ants as ants
 import nipype.interfaces.utility as utl
 import nipype.interfaces.io as nio
 # -------------------------------------------------------
 
-# -----------------Inputs--------------------------------
-# Define subject list, session list and relevent file types
-#working_dir = os.path.abspath('/mnt/hgfs/VMshare/WorkingBIDS/')
-working_dir = os.path.abspath(
-    '/run/media/mri/4e43a4f6-7402-4881-bcf5-d280e54cc385/Analysis/DCM2BIDS2')
-output_dir = os.path.join(working_dir, 'derivatives/')
-temp_dir = os.path.join(output_dir, 'datasink/')
-subject_list = ['02', '03', '04', '06', '08', '09', '10', '11']
-#subject_list =['11']
+fsl.FSLCommand.set_default_output_type('NIFTI')
 
-# directory: '\WorkingBIDS\derivatives\datasink\IntrasessionCoregister_T1w\sub-11\ses-Precon'
-session = 'Precon'
-# * precon T1w from IntersessionCoregister_preconScans
-filestart = 'sub-{subject_id}_ses-' + session + '_'
-scanfolder = 'IntersessionCoregister_preconUTEmean'
-subdirectory = os.path.join(temp_dir, scanfolder, 'sub-{subject_id}')
-precon_UTE_mean = os.path.join(subdirectory,
-                               'rmean' + filestart + '*fast*UTE*.nii')
-# + postcon scans
-session = 'Postcon'
-# * preprocessing (sub-??, ses-Postcon, qutece)
-scanfolder = 'preprocessing'
-subdirectory = os.path.join(temp_dir, scanfolder, 'sub-{subject_id}',
-                            'ses-' + session)
-filestart = 'sub-{subject_id}_ses-' + session + '_'
-postcon_UTE_files = os.path.join(subdirectory, 'qutece',
-                                 'r' + filestart + '*fast*UTE*.nii')
 
-templates = {
-    'qutece_precon_mean': precon_UTE_mean,
-    'qutece_postcon': postcon_UTE_files
-}
+def ScanDiff_workflow(working_dir, subject_list, session_list, num_cores,
+                      scan_type):
 
-# Infosource - a function free node to iterate over the list of subject names
-infosource = eng.Node(utl.IdentityInterface(fields=['subject_id']),
-                      name="infosource")
-infosource.iterables = [('subject_id', subject_list)]
+    # -----------------Inputs--------------------------------
+    # Define subject list, session list and relevent file types
+    # working_dir = os.path.abspath(
+    #    '/run/media/mri/4e43a4f6-7402-4881-bcf5-d280e54cc385/Analysis/DCM2BIDS2')
+    output_dir = os.path.join(working_dir, 'derivatives/')
+    temp_dir = os.path.join(output_dir, 'datasink/')
 
-# Selectfiles to provide specific scans with in a subject to other functions
-selectfiles = eng.Node(nio.SelectFiles(templates,
-                                       base_directory=working_dir,
-                                       sort_filelist=True,
-                                       raise_on_empty=True),
-                       name="SelectFiles")
-# -------------------------------------------------------
+    session = 'Precon'
+    # * precon T1w from IntersessionCoregister_preconScans
+    filestart = 'sub-{subject_id}_ses-' + session + '_'
+    scanfolder = 'IntersessionCoregister_preconScansSPM_SPM'
+    subdirectory = os.path.join(temp_dir, scanfolder, 'sub-{subject_id}')
 
-# -----------------------DiffNode-------------------
-difference = eng.MapNode(cnp.DiffNii(), name='difference', iterfield='file2')
-#difference.iterables = [('file1', ['precon_UTE_files']), ('file2', ['postcon_UTE_files'])] # this is pseudo code not real
-# -------------------------------------------------------
+    precon_UTE_files = os.path.join(
+        subdirectory, 'rrr' + filestart + '*' + scan_type + '*UTE*.nii')
+    # + postcon scans
+    session = 'Postcon'
+    # * preprocessing (sub-??, ses-Postcon, qutece)
+    scanfolder = 'preprocessing'
+    subdirectory = os.path.join(temp_dir, scanfolder, 'sub-{subject_id}',
+                                'ses-' + session)
+    filestart = 'sub-{subject_id}_ses-' + session + '_'
+    postcon_UTE_files = os.path.join(
+        subdirectory, 'qutece',
+        'r' + filestart + '*' + scan_type + '*UTE*.nii')
 
-# ------------------------Output-------------------------
-# Datasink - creates output folder for important outputs
-datasink = eng.Node(nio.DataSink(base_directory=output_dir,
-                                 container=temp_dir),
-                    name="datasink")
-# Use the following DataSink output substitutions
-substitutions = [('_subject_id_', 'sub-')]
+    templates = {
+        'qutece_pre': precon_UTE_files,
+        'qutece_post': postcon_UTE_files
+    }
 
-subjFolders = [('sub-%s' % (sub), 'sub-%s' % (sub)) for sub in subject_list]
-substitutions.extend(subjFolders)
-datasink.inputs.substitutions = substitutions
-datasink.inputs.regexp_substitutions = [('_difference.??/', '')]
-# -------------------------------------------------------
+    # Infosource - a function free node to iterate over the list of subject names
+    infosource = eng.Node(utl.IdentityInterface(fields=['subject_id']),
+                          name="infosource")
+    infosource.iterables = [('subject_id', subject_list)]
 
-# -----------------NormalizationWorkflow-----------------
-task = 'postminuspre'
-diff_wf = eng.Workflow(name=task)
-diff_wf.base_dir = working_dir + '/workflow'
+    # Selectfiles to provide specific scans with in a subject to other functions
+    selectfiles = eng.Node(nio.SelectFiles(templates,
+                                           base_directory=working_dir,
+                                           sort_filelist=True,
+                                           raise_on_empty=True),
+                           name="SelectFiles")
+    # -------------------------------------------------------
 
-diff_wf.connect([(infosource, selectfiles, [('subject_id', 'subject_id')])])
-diff_wf.connect([(selectfiles, difference, [('qutece_precon_mean', 'file1'),
-                                            ('qutece_postcon', 'file2')])])
+    # -----------------------------AverageImages-------------
+    average_niis = eng.Node(ants.AverageImages(), name='average_niis')
+    average_niis.inputs.dimension = 3
+    average_niis.inputs.normalize = False
+    # -------------------------------------------------------
 
-diff_wf.connect([(difference, datasink, [('out_file', task + '.@con')])])
-# -------------------------------------------------------
+    # -----------------------DiffNode-------------------
+    difference = eng.MapNode(cnp.DiffNii(),
+                             name='difference',
+                             iterfield='file2')
+    #difference.iterables = [('file1', ['precon_UTE_files']), ('file2', ['postcon_UTE_files'])] # this is pseudo code not real
+    # -------------------------------------------------------
 
-# -------------------WorkflowPlotting--------------------
-diff_wf.write_graph(graph2use='flat')
-# -------------------------------------------------------
+    # ------------------------Output-------------------------
+    # Datasink - creates output folder for important outputs
+    datasink = eng.Node(nio.DataSink(base_directory=output_dir,
+                                     container=temp_dir),
+                        name="datasink")
+    # Use the following DataSink output substitutions
+    substitutions = [('_subject_id_', 'sub-')]
 
-diff_wf.run(plugin='MultiProc', plugin_args={'n_procs': 5})
+    subjFolders = [('sub-%s' % (sub), 'sub-%s' % (sub))
+                   for sub in subject_list]
+    substitutions.extend(subjFolders)
+    datasink.inputs.substitutions = substitutions
+    datasink.inputs.regexp_substitutions = [('_difference.??/', '')]
+    # -------------------------------------------------------
+
+    # -----------------NormalizationWorkflow-----------------
+    task = 'postminuspre_' + scan_type
+    diff_wf = eng.Workflow(name=task)
+    diff_wf.base_dir = working_dir + '/workflow'
+
+    diff_wf.connect([(infosource, selectfiles, [('subject_id', 'subject_id')]),
+                     (selectfiles, average_niis, [('qutece_pre', 'images')]),
+                     (average_niis, difference, [('output_average_image',
+                                                  'file1')]),
+                     (selectfiles, difference, [('qutece_postcon', 'file2')]),
+                     (difference, datasink, [('out_file', task + '.@con')])])
+    # -------------------------------------------------------
+
+    # -------------------WorkflowPlotting--------------------
+    diff_wf.write_graph(graph2use='flat')
+    # -------------------------------------------------------
+
+    if num_cores < 2:
+        preproc_wf.run()
+    else:
+        preproc_wf.run(plugin='MultiProc', plugin_args={'n_procs': num_cores})
