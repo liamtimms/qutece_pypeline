4b9bd7afed7d72a0fb40072d7357848ebb995af5
Liam Timms <timms5000@gmail.com>
2020-03-25 20:40:01 -0400

    changes for Tianyi to work from

--- nipype_controller.py
+++ nipype_controller.py
@@ -3,6 +3,7 @@ from nipype_preproc import Preproc_workflow
 from nipype_coreg import IntrasesCoreg_workflow
 from nipype_coreg2 import IntersesCoreg_workflow
 from nipype_scan_diff import ScanDiff_workflow
+from nipype_normalize_compare import Normalization_workflow
 from textme import textme
 
 upper_dir = os.path.realpath('../..')
@@ -17,21 +18,21 @@ subject_list = ['02', '03', '04', '06', '08', '09', '10', '11']
 num_cores = 1
 Preproc_workflow(working_dir, subject_list, session_list, num_cores)
 os.system("notify-send Preprocessing done")
-# textme("done with Preprocessing")
 
 # Extremely important to not start too many FLIRTs in parrallel
 IntrasesCoreg_workflow(working_dir, subject_list, session_list, num_cores)
 os.system("notify-send IntrasessionCoregistration done")
-# textme("done with IntrasessionCoregistration")
 
 IntersesCoreg_workflow(working_dir, subject_list, num_cores)
-os.system("notify-send IntrasessionCoregistration done")
+os.system("notify-send IntersessionCoregistration done")
 
 num_cores = 1
-scan_type='hr'
-ScanDiff_workflow(working_dir, subject_list, session_list, num_cores, scan_type)
-scan_type='fast'
-ScanDiff_workflow(working_dir, subject_list, session_list, num_cores, scan_type)
+scan_type = 'hr'
+ScanDiff_workflow(working_dir, subject_list, session_list, num_cores,
+                  scan_type)
+scan_type = 'fast'
+ScanDiff_workflow(working_dir, subject_list, session_list, num_cores,
+                  scan_type)
 os.system("notify-send ScanDiff done")
 
 num_cores = 2
