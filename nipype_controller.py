3a7ff01b2b4a759c0880c811299b80e023cd2230
Liam Timms <timms5000@gmail.com>
2020-04-23 20:09:17 -0400

    as run in DCM2BIDS4

--- nipype_controller.py
+++ nipype_controller.py
@@ -3,7 +3,9 @@ from nipype_preproc import Preproc_workflow
 from nipype_coreg import IntrasesCoreg_workflow
 from nipype_coreg2 import IntersesCoreg_workflow
 from nipype_scan_diff import ScanDiff_workflow
-from nipype_normalize_compare import Normalization_workflow
+from nipype_normalize_braincrop import BrainCrop_workflow
+from nipype_normalize_semiauto import Normalization_workflow
+from nipype_normalize_applytrans import ApplyTrans_workflow
 from textme import textme
 
 upper_dir = os.path.realpath('../..')
@@ -12,13 +14,15 @@ session_list = ['Precon', 'Postcon']
 
 subject_list = ['03', '11']
 subject_list = ['02', '04', '08', '10', '11']
-subject_list = ['02', '03', '04', '06', '08', '09', '10', '11']
-# subject_list = ['06', '11']
+# subject_list = ['02', '03', '04', '06', '08', '09', '10', '11']
+subject_list = ['03', '04', '06', '11']
+#subject_list = ['11']
 
 num_cores = 1
-Preproc_workflow(working_dir, subject_list, session_list, num_cores)
-os.system("notify-send Preprocessing done")
+# Preproc_workflow(working_dir, subject_list, session_list, num_cores)
+# os.system("notify-send Preprocessing done")
 
+num_cores = 1
 # Extremely important to not start too many FLIRTs in parrallel
 IntrasesCoreg_workflow(working_dir, subject_list, session_list, num_cores)
 os.system("notify-send IntrasessionCoregistration done")
@@ -26,7 +30,6 @@ os.system("notify-send IntrasessionCoregistration done")
 IntersesCoreg_workflow(working_dir, subject_list, num_cores)
 os.system("notify-send IntersessionCoregistration done")
 
-num_cores = 1
 scan_type = 'hr'
 ScanDiff_workflow(working_dir, subject_list, session_list, num_cores,
                   scan_type)
@@ -35,5 +38,20 @@ ScanDiff_workflow(working_dir, subject_list, session_list, num_cores,
                   scan_type)
 os.system("notify-send ScanDiff done")
 
-num_cores = 2
+BrainCrop_workflow(working_dir, subject_list, num_cores)
+os.system("notify-send BrainCrop done")
+
+## AT THIS POINT MANUAL MASKS MUST BE COMPLETED USING THE BRAIN CROPPED IMAGES
+num_cores = 4
 Normalization_workflow(working_dir, subject_list, num_cores)
+os.system("notify-send Norm done")
+
+num_cores = 4
+scan_type = 'hr'
+ApplyTrans_workflow(working_dir, subject_list, session_list, num_cores,
+                    scan_type)
+# # scan_type = 'fast'
+# # ApplyTrans_workflow(working_dir, subject_list, session_list, num_cores,
+# #                     scan_type)
+#
+# os.system("notify-send Transforms done")
