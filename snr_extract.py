ee4ecc2d818f4dff69063623827023b1dc02f9c2
Liam Timms <timms5000@gmail.com>
2020-06-10 10:13:44 -0400

    adition of artery and vein segmenation for kidney

--- snr_extract.py
+++ snr_extract.py
@@ -3,23 +3,26 @@ import numpy as np
 import pandas as pd
 import os
 import json
-import CustomNiPype as cnp
-import nipype.pipeline.engine as eng
 from nipype.utils.filemanip import split_filename
 
-base_path = '/home/liam/LaptopSync/DCM2BIDS_Kidney'
+base_path = '../..'
 subject_list = ['3', '4', '5', '6']
+subject_list = ['8']
 #subject_list = ['3', '4']
 
 for sub_num in subject_list:
     data_dir = os.path.join(base_path, 'sub-' + sub_num, 'ses-Post', 'qutece')
     snr_mask_dir = os.path.join(base_path, 'derivatives', 'manual-work', 'snr',
                                 'sub-' + sub_num)
+    av_mask_dir = os.path.join(base_path, 'derivatives', 'manual-work',
+                               'segmentation-vesselness_ArteryAndVein',
+                               'sub-' + sub_num)
     n = 0
 
     sub_vals = [[
-        'Scan Name', 'Blood Intensity', 'Intraluminal Signal Homogeneity',
-        'SNR', 'CNR', 'Voxel Size', 'TE', 'TR', 'FA'
+        'Scan Name', 'Artery Intensity', 'Artery ISH', 'Artery SNR',
+        'Artery CNR', 'Vein Intensity', 'Vein ISH', 'Vein SNR', 'Vein CNR',
+        'Voxel Size', 'TE', 'TR', 'FA'
     ]]
 
     for filename in os.listdir(data_dir):
@@ -43,14 +46,18 @@ for sub_num in subject_list:
 
             snr_fname = 'SNR_' + fname + '_Segmentation-label.nii'
             print(snr_fname)
+            av_fname = fname + '_VesselnessFiltered_final_Segmentation-label.nii'
 
             snr_filename = os.path.join(snr_mask_dir, snr_fname)
             snr_ROI_nii = nib.load(snr_filename)
             snr_ROI_img = np.array(snr_ROI_nii.get_fdata())
 
-            corr_fname = fname + '_corrected.nii'
-            snr_ROI_nii = nib.load(snr_filename)
-            snr_ROI_img = np.array(snr_ROI_nii.get_fdata())
+            av_filename = os.path.join(av_mask_dir, av_fname)
+            av_ROI_nii = nib.load(av_filename)
+            av_ROI_img = np.array(av_ROI_nii.get_fdata())
+
+            # snr_ROI_img is labelmap for blood, noise and tissue, while
+            #   av_ROI_img is labelmap for aorta and IVC
 
             out = ([int(sub_num), n])
 
@@ -64,11 +71,14 @@ for sub_num in subject_list:
                 vals = np.reshape(crop_img, -1)
                 ave = np.nanmean(vals)
                 std = np.nanstd(vals)
+
+                print('Now processing noise & tissue labelmap......')
                 print('for ' + str(r))
                 print('ave = ' + str(ave) + ' with ' + str(len(vals)) +
                       ' points')
                 print('std = ' + str(std))
                 out.append([ave, std])
+
                 if r == 1:
                     blood_signal = ave
                     blood_noise = std
@@ -77,19 +87,50 @@ for sub_num in subject_list:
                 elif r == 3:
                     tissue = ave
 
-            snr = blood_signal / noise
-            ish = blood_noise / blood_signal
+            for r in np.unique(av_ROI_img):
+                roi = (av_ROI_img == r).astype(int)
+                roi = roi.astype('float')
+                roi[roi ==
+                    0] = np.nan  #zero can be true value so mask with nan
+
+                crop_img = np.multiply(scan_img, roi)
+                vals = np.reshape(crop_img, -1)
+                ave = np.nanmean(vals)
+                std = np.nanstd(vals)
+
+                print('Now processing artery & vein labelmap......')
+                print('for ' + str(r))
+                print('ave = ' + str(ave) + ' with ' + str(len(vals)) +
+                      ' points')
+                print('std = ' + str(std))
+                out.append([ave, std])
+
+                if r == 1:
+                    artery_signal = ave
+                    artery_noise = std
+                elif r == 2:
+                    vein_signal = ave
+                    vein_noise = std
+
+            snr_artery = artery_signal / noise
+            snr_vein = vein_signal / noise
+            ish_artery = artery_noise / artery_signal
+            ish_vein = vein_noise / vein_signal
+
             snr_tissue = tissue / noise
-            cnr = snr - snr_tissue
-            scan_vals = [fname, blood_signal, ish, snr, cnr, voxelsize, TE, TR, FA]
+            cnr_artery = snr_artery - snr_tissue
+            cnr_vein = snr_vein - snr_tissue
+            scan_vals = [
+                fname, artery_signal, ish_artery, snr_artery, cnr_artery,
+                vein_signal, ish_vein, snr_vein, cnr_vein, voxelsize, TE, TR,
+                FA
+            ]
             sub_vals.append(scan_vals)
-            print('for ' + snr_fname)
-            print('SNR = ' + str(snr))  #+ '\n')
-            print('CNR = ' + str(cnr) + '\n')
-            del blood_signal, ish, noise, tissue
+
+            del artery_signal, artery_noise, vein_signal, vein_noise, noise, tissue
             n = n + 1
 
-        scan_vals_csv_filename = os.path.join(snr_mask_dir,
-                                              'snr_sub-' + sub_num + '.csv')
+        scan_vals_csv_filename = os.path.join(
+            av_mask_dir, 'snr_arteryvein_sub-' + sub_num + '.csv')
         export = pd.DataFrame(sub_vals)
         export.to_csv(scan_vals_csv_filename, index=False)
