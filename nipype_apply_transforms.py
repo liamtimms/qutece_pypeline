12648b32b331fced6ff3698ecc887ea1e83fd56d
Liam Timms <timms5000@gmail.com>
2020-07-31 12:59:19 -0400

    added nonlinear transformation application

--- nipype_apply_transforms.py
+++ nipype_apply_transforms.py
@@ -4,6 +4,7 @@ import os
 import CustomNiPype as cnp
 import nipype.pipeline.engine as eng
 import nipype.interfaces.fsl as fsl
+import nipype.interfaces.spm as spm
 import nipype.interfaces.utility as utl
 import nipype.interfaces.io as nio
 # -------------------------------------------------------
@@ -144,6 +145,148 @@ def apply_linear_trans(working_dir, subject_list, scan_type):
         (apply_linear, datasink, [('out_file', task + '.@con')]),
         (apply_linear, applymask, [('out_file', 'in_file')]),
         (applymask, plot_dist, [('out_file', 'in_files')]),
+        (plot_dist, datasink, [('out_fig', task + '_plots.@con')])
+    ])
+    # -------------------------------------------------------
+
+    return trans_wf
+
+
+def apply_nonlinear_trans(working_dir, subject_list, scan_type):
+
+    # -----------------Inputs--------------------------------
+    output_dir = os.path.join(working_dir, 'derivatives/')
+    temp_dir = os.path.join(output_dir, 'datasink/')
+
+    fsl_dir = '/opt/fsl/data/standard/'
+    MNI_file = os.path.join(fsl_dir, 'MNI152_T1_1mm.nii.gz')
+    MNI_brain_file = os.path.join(fsl_dir, 'MNI152_T1_1mm_brain.nii.gz')
+    MNI_mask_file = os.path.join(fsl_dir,
+                                 'MNI152_T1_1mm_brain_mask_dil.nii.gz')
+
+    spm_dir = '/opt/spm12/tpm/'
+    SPM_atlas_file = os.path.join(spm_dir, 'labels_Neuromorphometrics.nii')
+
+    filestart = 'sub-{subject_id}_ses-Precon'
+    # Transforms
+    scanfolder = 'calc_transforms_nonlinear_trans'
+    subdirectory = os.path.join(temp_dir, scanfolder, 'sub-{subject_id}')
+    warp_field_files = os.path.join(subdirectory,
+                                    'rrr' + filestart + '*_field.nii')
+
+    # * precon T1w from IntersessionCoregister_preconScans
+    session = 'Precon'
+    filestart = 'sub-{subject_id}_ses-' + session + '_'
+    subdirectory = os.path.join(temp_dir, scanfolder, 'sub-{subject_id}')
+
+    # UTE Files
+    scanfolder = 'linear_transfomed_' + scan_type
+    subdirectory = os.path.join(temp_dir, scanfolder, 'sub-{subject_id}')
+    filestart = 'sub-{subject_id}'
+    UTE_files = os.path.join(subdirectory,
+                             '_r*' + filestart + '*' + scan_type + '*UTE*.nii')
+
+    templates = {
+        'mni_head': MNI_file,
+        'mni_mask': MNI_mask_file,
+        'mni_brain': MNI_brain_file,
+        'spm_atlas': SPM_atlas_file,
+        'warp_field': warp_field_files,
+        'UTE': UTE_files
+    }
+
+    # Infosource - function free node to iterate over the list of subject names
+    infosource = eng.Node(utl.IdentityInterface(fields=['subject_id']),
+                          name="infosource")
+    infosource.iterables = [('subject_id', subject_list)]
+
+    # Selectfiles to provide specific scans within a subject to other functions
+    selectfiles = eng.Node(nio.SelectFiles(templates,
+                                           base_directory=working_dir,
+                                           sort_filelist=True,
+                                           raise_on_empty=True),
+                           name="SelectFiles")
+    # -------------------------------------------------------
+
+    # FSL
+    # ---------------------FixNANs----------------------
+    maths = eng.MapNode(fsl.maths.MathsCommand(),
+                        name='maths',
+                        iterfield=['in_file'])
+    maths.inputs.nan2zeros = True
+    maths.inputs.output_type = 'NIFTI'
+    # -------------------------------------------------------
+
+    # -----------------CropBrainFixNaNs----------------------
+    applymask = eng.MapNode(fsl.ApplyMask(),
+                            name='applymask',
+                            iterfield='in_file')
+    applymask.inputs.nan2zeros = True
+    applymask.inputs.output_type = 'NIFTI'
+    # -------------------------------------------------------
+
+    # -----------------LinearTransform--------------------
+    apply_nonlinear = eng.MapNode(fsl.ApplyWarp(),
+                                  name='apply_nonlinear',
+                                  iterfield=['in_file'])
+
+    apply_nonlinear.inputs.interp = 'spline'
+    apply_nonlinear.inputs.output_type = 'NIFTI'
+    # -------------------------------------------------------
+
+    # ----------------------DivideNii------------------------
+    plot_dist = eng.Node(interface=cnp.PlotDistribution(), name='plot_dist')
+    plot_dist.inputs.plot_xlim_max = 500
+    plot_dist.inputs.plot_xlim_min = 10
+    # -------------------------------------------------------
+
+    # --------------SPM_Reslice-------------------
+    reslice = eng.MapNode(interface=spm.Reslice(),
+                          name='reslice',
+                          iterfield=['in_file'])
+    # -------------------------------------------------------
+
+    # --------------ROI_Analyze_WholeBrain-------------------
+    roi_analyze_region = eng.MapNode(interface=cnp.ROIAnalyze(),
+                                     name='roi_analyze_region',
+                                     iterfield=['scan_file'])
+    # -------------------------------------------------------
+
+    # ------------------------Output-------------------------
+    # Datasink - creates output folder for important outputs
+    datasink = eng.Node(nio.DataSink(base_directory=output_dir,
+                                     container=temp_dir),
+                        name="datasink")
+    # Use the following DataSink output substitutions
+    substitutions = [('_subject_id_', 'sub-')]
+
+    subjFolders = [('sub-%s' % (sub), 'sub-%s' % (sub))
+                   for sub in subject_list]
+    substitutions.extend(subjFolders)
+    datasink.inputs.substitutions = substitutions
+    datasink.inputs.regexp_substitutions = [('apply_nonlinear.*/', ''), ('_roi_analyze_region.*/', '')]
+    # -------------------------------------------------------
+
+    # -----------------NormalizationWorkflow-----------------
+    task = 'nonlinear_transfomed_' + scan_type
+    trans_wf = eng.Workflow(name=task)
+    trans_wf.base_dir = working_dir + '/workflow'
+
+    trans_wf.connect([
+        (infosource, selectfiles, [('subject_id', 'subject_id')]),
+        (selectfiles, apply_nonlinear, [('mni_brain', 'ref_file'),
+                                        ('warp_field', 'field_file')]),
+        (selectfiles, maths, [('UTE', 'in_file')]),
+        (selectfiles, reslice, [('spm_atlas', 'space_defining')]),
+        (selectfiles, applymask, [('mni_mask', 'mask_file')]),
+        (maths, apply_nonlinear, [('out_file', 'in_file')]),
+        (apply_nonlinear, datasink, [('out_file', task + '.@con')]),
+        (apply_nonlinear, applymask, [('out_file', 'in_file')]),
+        (applymask, plot_dist, [('out_file', 'in_files')]),
+        (apply_nonlinear, reslice, [('out_file', 'in_file')]),
+        (selectfiles, roi_analyze_region, [('spm_atlas', 'roi_file')]),
+        (reslice, roi_analyze_region, [('out_file', 'scan_file')]),
+        (roi_analyze_region, datasink, [('out_file', task + '_csv.@con')]),
         (plot_dist, datasink, [('out_fig', task + '_plots.@con')])
     ])
     # -------------------------------------------------------
