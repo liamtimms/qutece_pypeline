e542a9249d1cf538e1f2439fd1d40807ffa594ec
Liam Timms <timms5000@gmail.com>
2020-08-13 09:49:29 -0400

    add in blog parameter and apply it to multiple brain data subjects

--- vmtk_vesselness_param_explorer.py
+++ vmtk_vesselness_param_explorer.py
@@ -1,14 +1,16 @@
-import vmtk
+# import vmtk
 from vmtk import pypes
 import os
 import nibabel as nib
 
 
 def SetAlpha(suppressPlates):
-    return 0.000 + 3.0 * pow(suppressPlates/100.0,2)
+    return 0.000 + 3.0 * pow(suppressPlates / 100.0, 2)
+
 
 def SetBeta(suppressBlobs):
-    return 0.001 + 1.0 * pow((100.0-suppressBlobs)/100.0,2)
+    return 0.001 + 1.0 * pow((100.0 - suppressBlobs) / 100.0, 2)
+
 
 def GetPixelSpacing(nii_filename):
     # minimum spacing is used in vmtkSlicerExtension
@@ -16,43 +18,64 @@ def GetPixelSpacing(nii_filename):
     pixdim = nii.header['pixdim']
     return min(pixdim[1:3])
 
+
 # ----------------------- Input ------------------------------------------
-subject_num= '8'
-infile = 'sub-8_ses-Post_SPIRiT_un_DIS3D_run-01_UTE'
+subject_list = ['02', '03', '04', '06', '08', '11', '12', '13', '14', '15']
 
-basefolder = '/media/tianyi/data/Analysis/DCM2BIDS_Kidney/'
-scanfolder = 'sub-' + subject_num + '/ses-Post/qutece/'
-outfolder = 'derivatives/manual-work/vesselness-filtered/sub-' + subject_num + '/'
+for subject_num in subject_list:
 
-inVesselness = basefolder + scanfolder + infile + '.nii'
+    upper_dir = os.path.realpath('../..')
+    basefolder = os.path.abspath(os.path.join(upper_dir, 'derivatives'))
 
-pixelspacing = GetPixelSpacing(inVesselness)
-voxelmin = 1
-voxelmax = 4
-sigmamin = pixelspacing * voxelmin
-sigmamax = pixelspacing * voxelmax
-sigmasteps = 5      # default = 5 in vmtkSlicerExtension
+    scanfolder = os.path.join(basefolder, 'datasink', 'preprocessing',
+                              'sub-' + subject_num, 'ses-Postcon', 'qutece')
 
-suppressPlates = 25
-suppressBlobs = 25
-alpha = SetAlpha(suppressPlates)
-beta = SetBeta(suppressBlobs)
-gamma = 300
+    outfolder = os.path.join(basefolder, 'manualwork', 'vesselness_filtered',
+                             'sub-' + subject_num)
 
-# --------------------- Param Space --------------------------------------
-voxelmax_list = [4]
-gamma_list = [50, 75, 100, 125, 150]
-params_space = [(vmax, g) for vmax in voxelmax_list for g in gamma_list]
+    print(outfolder)
+    if not os.path.exists(outfolder):
+        os.makedirs(outfolder)
 
-for params in params_space:
-    voxelmax, gamma = params
+    infile = 'rsub-' + subject_num + '_ses-Postcon_hr_run-01_UTE_desc-preproc'
+    inVesselness = os.path.join(basefolder, scanfolder, infile + '.nii')
+    print(inVesselness)
+
+    pixelspacing = GetPixelSpacing(inVesselness)
+    voxelmin = 1
+    voxelmax = 4
+    sigmamin = pixelspacing * voxelmin
     sigmamax = pixelspacing * voxelmax
+    sigmasteps = 5  # default = 5 in vmtkSlicerExtension
 
-    outVesselness = basefolder + outfolder + infile + '_AutoVesselness' + '_vmax=' + str(voxelmax) + '_gamma=' + str(gamma) + '.nii'
+    suppressPlates = 25
+    suppressBlobs = 25
+    alpha = SetAlpha(suppressPlates)
+    beta = SetBeta(suppressBlobs)
+    gamma = 300
 
-    cmdVesselness = 'vmtkimagecast -type float -ifile {} --pipe vmtkimagevesselenhancement -ofile {} -method frangi -sigmamin {} -sigmamax {} -sigmasteps {} -alpha {} -beta {} -gamma {}'.format(inVesselness,outVesselness,sigmamin,sigmamax,sigmasteps,alpha,beta,gamma)
-    pypes.PypeRun(cmdVesselness)
+    # --------------------- Param Space --------------------------------------
+    suppressBlobs_list = [5, 25, 100]
+    gamma_list = [50, 100, 200]
+    # params_space = [(vmax, g) for vmax in voxelmax_list for g in gamma_list]
+    params_space = [(suppressBlobs, g) for suppressBlobs in suppressBlobs_list
+                    for g in gamma_list]
 
+    for params in params_space:
+        suppressBlobs, gamma = params
+        sigmamax = pixelspacing * voxelmax
 
+        beta = SetBeta(suppressBlobs)
+        outVesselness = os.path.join(
+            outfolder, infile + '_AutoVesselness' + '_sblobs=' +
+            str(suppressBlobs) + '_gamma=' + str(gamma) + '.nii')
+        print(outVesselness)
 
+        cmdVesselness = (
+            'vmtkimagecast -type float -ifile {} --pipe vmtkimagevesselenhancement -ofile {} '
+            +
+            '-method frangi -sigmamin {} -sigmamax {} -sigmasteps {} -alpha {} -beta {} -gamma {}'
+        ).format(inVesselness, outVesselness, sigmamin, sigmamax, sigmasteps,
+                 alpha, beta, gamma)
 
+        pypes.PypeRun(cmdVesselness)
