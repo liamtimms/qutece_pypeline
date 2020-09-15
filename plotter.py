b9d0e3fa39a2de099cb6ce56ebea7f5b3b224d35
Liam Timms <timms5000@gmail.com>
2020-09-15 16:55:59 -0400

    added roi_cut, added subject_summary_vesselness

--- plotter.py
+++ plotter.py
@@ -14,7 +14,22 @@ datasink_dir = os.path.join(base_dir, 'derivatives', 'datasink')
 manualwork_dir = os.path.join(base_dir, 'derivatives', 'manualwork')
 
 
-def roi_extract(scan_img, roi_img):
+def roi_cut(scan_img, roi_img, t, r):
+    if t == 'equal':
+        roi = (roi_img == r).astype(int)
+    elif t == 'greater':
+        roi = (roi_img > r).astype(int)
+    else:
+        print('need valid roi cut type')
+
+    roi = roi.astype('float')
+    # zero can be a true value so mask with nan
+    roi[roi == 0] = np.nan
+    crop_img = np.multiply(scan_img, roi)
+    return crop_img
+
+
+def roi_extract(scan_img, roi_img, t='equal'):
 
     vals_df_list = []
     unique_roi = np.unique(roi_img)
@@ -22,14 +37,7 @@ def roi_extract(scan_img, roi_img):
     n = 0
     extracted_df = pd.DataFrame()
     for r in unique_roi:
-        # r is label in roi
-        # n counts for number of labels
-
-        roi = (roi_img == r).astype(int)
-        roi = roi.astype('float')
-        # zero can be a true value so mask with nan
-        roi[roi == 0] = np.nan
-        crop_img = np.multiply(scan_img, roi)
+        crop_img = roi_cut(scan_img, roi_img, t, r)
         vals = np.reshape(crop_img, -1)
         vals_df = pd.DataFrame(vals)
         vals_df.dropna(inplace=True)
@@ -60,7 +68,7 @@ def hist_plots(df, seg_type, save_dir):
 
     for i, col in enumerate(df.columns):
         vals = df[col].to_numpy()
-        w = 1
+        w = 50
         n = math.ceil((np.nanmax(vals) - np.nanmin(vals)) / w)
 
         if n > 0:
@@ -74,7 +82,7 @@ def hist_plots(df, seg_type, save_dir):
                              'histtype': 'step',
                              'linewidth': 1
                          })
-            plt.xlim(-5, 50)
+            plt.xlim(-10, 4000)
 
             plt.savefig(out_fig_name, dpi=300, bbox_inches='tight')
             # plt.close(i)
@@ -111,6 +119,7 @@ def category_plot(postcon_df, precon_df, save_dir, sub_num, seg_type):
 
     return
 
+
 def swarm_plot(filt_df, save_dir, seg_type):
     filt_df = filt_df.reset_index()
     print(filt_df.head())
@@ -128,6 +137,7 @@ def swarm_plot(filt_df, save_dir, seg_type):
     sns_plot.savefig(os.path.join(save_dir, save_name))
 
     return
+
 
 def session_summary(in_folder, sub_num, session, scan_type, seg_type):
     data_dir = os.path.join(datasink_dir, in_folder, 'sub-{}'.format(sub_num),
@@ -153,12 +163,13 @@ def session_summary(in_folder, sub_num, session, scan_type, seg_type):
             '_noise-Segmentation-label.nii')
     elif seg_type == 'brain_preFLIRT':
         ROI_file_name = os.path.join(
-            roi_dir,
-            'rrrsub-' + sub_num + '_ses-Precon_T1w_*brain*' +
+            roi_dir, 'rrrsub-' + sub_num + '_ses-Precon_T1w_*brain*' +
             '_Segmentation-label.nii')
         ROI_file_names = glob.glob(ROI_file_name)
         ROI_file_name = ROI_file_names[0]
         print(ROI_file_names[0])
+    elif seg_type == 'vesselness':
+        print('Switch to session_summary_vesselness')
     else:
         print('need valid segmentation type')
 
@@ -176,6 +187,7 @@ def session_summary(in_folder, sub_num, session, scan_type, seg_type):
         scan_file_name = f
         scan_file_nii = nib.load(scan_file_name)
         scan_img = np.array(scan_file_nii.get_fdata())
+        pth, fname, ext = split_filename(f)
 
         if scan_img.size != roi_img.size:
             print('RESAMPLING')
@@ -201,7 +213,118 @@ def session_summary(in_folder, sub_num, session, scan_type, seg_type):
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
 
+        # save_name = ('sub-{}_ses-{}_' + scan_type + '_data_' +
+        save_name = (fname + '_DATA_' + 'seg-{}.csv').format(seg_type)
+        extracted_df.to_csv(os.path.join(save_dir, save_name), index=False)
+
+        save_name = (fname + '_SUMMARY_' + 'seg-{}.csv').format(seg_type)
+        summary_df.to_csv(os.path.join(save_dir, save_name))
+
+        summary_df_list.append(summary_df)
+
+    plt.close('all')
+    return summary_df_list
+
+
+def session_summary_vesselness(in_folder, sub_num, session, scan_type,
+                               seg_type):
+
+    if seg_type != 'vesselness':
+        print('switch to normal session_summary function')
+
+    data_dir = os.path.join(datasink_dir, in_folder, 'sub-{}'.format(sub_num),
+                            'ses-{}'.format(session), 'qutece')
+
+    if not os.path.exists(data_dir):
+        data_dir = os.path.join(datasink_dir, in_folder,
+                                'sub-{}'.format(sub_num),
+                                'ses-{}'.format(session))
+
+    if not os.path.exists(data_dir):
+        data_dir = os.path.join(datasink_dir, in_folder,
+                                'sub-{}'.format(sub_num))
+    # load brain mask
+    roi_dir = os.path.join(manualwork_dir, 'segmentations', 'brain_dil')
+    ROI_file_name = os.path.join(
+        roi_dir, 'rrrsub-' + sub_num + '_ses-Precon_T1w_*brain*' +
+        '_Segmentation-label.nii')
+
+    ROI_file_names = glob.glob(ROI_file_name)
+    ROI_file_name = ROI_file_names[0]
+    print(ROI_file_names[0])
+    ROI_file_nii = nib.load(ROI_file_name)
+    roi_img = np.array(ROI_file_nii.get_fdata())
+
+    session_pattern = '*rsub*' + session + '*' + scan_type + '*'
+    path_pattern = os.path.join(data_dir, session_pattern)
+    nii_files = glob.glob(path_pattern)
+    print('Selected files are: ')
+    print(nii_files)
+
+    summary_df_list = []
+
+    vesselness_dir = os.path.join(manualwork_dir, 'vesselness_filtered_2',
+                                  'sub-{}'.format(sub_num))
+
+    cut_off = 0.95
+
+    for f in nii_files:
+        scan_file_name = f
+        scan_file_nii = nib.load(scan_file_name)
+        scan_img = np.array(scan_file_nii.get_fdata())
+
         pth, fname, ext = split_filename(f)
+
+        if scan_img.size != roi_img.size:
+            print('RESAMPLING')
+            resampled_nii = nil.image.resample_to_img(scan_file_nii,
+                                                      ROI_file_nii)
+            scan_img = np.array(resampled_nii.get_fdata())
+
+        # load vesselness for scan
+        if session == 'Postcon':
+            vessel_fname = fname + '_AutoVess_g=*_sb=25_sp=10.nii'
+
+        elif session == 'Precon':
+            vessel_fname = ('rsub-' + sub_num +
+                            '_ses-Postcon_hr_run-01_UTE_desc-preproc' +
+                            '_AutoVess_g=*_sb=25_sp=10.nii')
+
+        vessel_file_name_pattern = os.path.join(vesselness_dir, vessel_fname)
+        vessel_file_name = glob.glob(vessel_file_name_pattern)
+        print(fname)
+        print('possible vesselness:')
+        print(vessel_file_name)
+        vessel_file_name = vessel_file_name[0]
+        vessel_nii = nib.load(vessel_file_name)
+        vessel_img = np.array(vessel_nii.get_fdata())
+
+        # crop initial brain from both scan and vesselness
+        t = 'equal'
+        r = 1
+        scan_img = roi_cut(scan_img, roi_img, t, r)
+        vessel_img = roi_cut(vessel_img, roi_img, t, r)
+
+        # construct vesselness roi
+        vessel_roi_img = (vessel_img > cut_off).astype(int)
+
+        extracted_df = roi_extract(scan_img, vessel_roi_img)
+
+        save_dir = os.path.join(datasink_dir, 'plots',
+                                'sub-{}'.format(sub_num),
+                                'ses-{}'.format(session))
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        hist_plots(extracted_df, seg_type, save_dir)
+
+        summary_df = extracted_df.describe()
+        print(summary_df)
+
+        save_dir = os.path.join(datasink_dir, 'csv_work_2',
+                                'sub-{}'.format(sub_num),
+                                'ses-{}'.format(session))
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
 
         # save_name = ('sub-{}_ses-{}_' + scan_type + '_data_' +
         save_name = (fname + '_DATA_' + 'seg-{}.csv').format(seg_type)
@@ -254,7 +377,7 @@ def subject_summary(sub_num, scan_type, seg_type):
 
 
 def full_summary(datasink_dir, subject_list, scan_type, seg_type):
-    csv_dir = 'csv_work_2'
+    # csv_dir = 'csv_work_2'
     df_list = []
     for sub_num in subject_list:
         postcon_df, precon_df = subject_summary(sub_num, scan_type, seg_type)
@@ -266,12 +389,19 @@ def full_summary(datasink_dir, subject_list, scan_type, seg_type):
 
     full_df = pd.concat(df_list)
     full_df = full_df.reset_index()
-    print('full_df : ' )
+    print('full_df : ')
     print(full_df.head())
-    filt_df = full_df.loc[(full_df['index'] == '1.0')]
+    filt_df = full_df.loc[(full_df['index'] == '1')]
     print(filt_df.head())
     save_dir = os.path.join(datasink_dir, 'plots')
     swarm_plot(filt_df, save_dir, seg_type)
+
+
+def snr(vessel_df, noise_df):
+    # signal = full_df.loc[(vessel_df['index'] == '1')]
+    return
+
+
 #
 #     stats_df = intensity_stats(csv_files)
 #
@@ -298,7 +428,6 @@ subject_list = [
 # subject_list = ['11']
 session_list = ['Postcon', 'Precon']
 scan_type = 'hr'
-# seg_type = 'Vesselness'
 in_folder = 'nonlinear_transfomed_hr'
 
 # seg_type = 'Neuromorphometrics'
@@ -312,7 +441,8 @@ in_folder = 'nonlinear_transfomed_hr'
 # subject_list = ['11']
 
 seg_type = 'noise'
-# seg_type = 'brain_preFLIRT'
+seg_type = 'brain_preFLIRT'
+
 # for sub_num in subject_list:
 #     session = 'Precon'
 #     in_folder = 'pre_to_post_coregister'
@@ -323,6 +453,26 @@ seg_type = 'noise'
 #     session_summary(in_folder, sub_num, session, scan_type, seg_type)
 #
 #     subject_summary(sub_num, scan_type, seg_type)
+# full_summary(datasink_dir, subject_list, scan_type, seg_type)
+
+subject_list = ['06', '07', '08', '10', '11', '12', '13', '14', '15']
+seg_type = 'vesselness'
+# subject_list = ['02', '03', '04', '05']
+# for sub_num in subject_list:
+#     session = 'Postcon'
+#     in_folder = 'preprocessing'
+#     session_summary_vesselness(in_folder, sub_num, session, scan_type,
+#                                seg_type)
+#     session = 'Precon'
+#     in_folder = 'pre_to_post_coregister'
+#     session_summary_vesselness(in_folder, sub_num, session, scan_type,
+#                                seg_type)
+#     # subject_summary(sub_num, scan_type, seg_type)
+
+subject_list = [
+    '02', '03', '04', '05', '06', '07', '08', '10', '11', '12', '13', '14',
+    '15'
+]
 full_summary(datasink_dir, subject_list, scan_type, seg_type)
 
 #     roi_nii
